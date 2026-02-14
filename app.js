8e44a132fdf7610c3a98c362e5f0b62d89614e28
exumm <brandyemilio20@gmail.com>
2026-02-14 18:54:45 -0400

    Update app.js

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ setTimeout(() => {
 }, 10000);
 
 // FECHA DE INICIO FIJA: Lunes 9 de Febrero a las 10:00 PM (Hora RD)
-const unlockStart = new Date("February 9, 2026 20:00:00").getTime();
+const unlockStart = new Date("February 9, 2026 19:00:00").getTime();
 
 function updateLogic() {
     const now = Date.now();
@@ -163,5 +163,6 @@ function lanzarFuegos() {
     }
     anim();
 }
+
 
 
